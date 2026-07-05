3b51649a61976a1e941c5b9ee24475e1238772f1
Daria Teodora Harabor <157858696+daria-harabor@users.noreply.github.com>
2026-07-05 11:48:10 -0400

    Add: coin-change - submission-5

---
+++ Data Structures & Algorithms/coin-change/submission-5.py
@@ -0,0 +1,17 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        no_coins = [-1 for i in range(amount + 1)]
+        no_coins[0] = 0
+        for i in range(1, amount + 1):
+            minim = 2**31
+
+            for c in coins:
+                if c <= i and no_coins[i-c] != -1 and no_coins[i-c] < minim:
+                    minim = no_coins[i-c]
+            if minim < 2**31:
+                no_coins[i] = minim + 1
+
+        return no_coins[amount]
+    
+
+          
