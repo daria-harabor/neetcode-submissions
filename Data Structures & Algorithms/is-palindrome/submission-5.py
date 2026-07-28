addfa6852c11e8d5901ee41778b56a745290594f
Daria Teodora Harabor <157858696+daria-harabor@users.noreply.github.com>
2026-07-28 01:27:28 -0400

    Add: is-palindrome - submission-5

---
+++ Data Structures & Algorithms/is-palindrome/submission-5.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        clean_s = str()
+        for char in s:
+            if char.isalnum() == True:
+                clean_s = clean_s + char
+
+        reverse_clean_s = str() 
+        length = len(clean_s)
+
+        for i in range(length):
+            reverse_clean_s = reverse_clean_s + clean_s[length-i-1]
+
+        if reverse_clean_s.casefold() == clean_s.casefold():
+            return True
+        else:
+            return False
+        
